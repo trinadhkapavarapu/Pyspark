3d605391b1dca72dbd359397dda7395459bd05ac
srinivas.adf119@gmail.com <srinivas.adf119@gmail.com>
2026-08-11 20:54:26 +0000

    yes

--- NOTEBOOKS/Read csv file into dataframe using pyspark.py
+++ NOTEBOOKS/Read csv file into dataframe using pyspark.py
@@ -1,4 +1,8 @@
 # Databricks notebook source
+# /// script
+# [tool.databricks.environment]
+# environment_version = "5"
+# ///
 # MAGIC %md
 # MAGIC
 # MAGIC
@@ -6,3 +10,18 @@
 
 # COMMAND ----------
 
+# DBTITLE 1,#Formate1:
+df = spark.read.csv(path='dbfs:/filestore/data/employ.csv',header= True)
+display(df)
+df.printSchema()
+
+
+
+# COMMAND ----------
+
+# DBTITLE 1,Formate2:
+df = spark.read.format('csv') \
+    .options(header='true', inferSchema='true') \
+    .load('/dbfs/mnt/data/employ.csv')
+display(df)
+df.printSchema()
